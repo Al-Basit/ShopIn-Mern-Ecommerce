0ea00195c2b61651d646e0089336e26f53b5ce05
Al-Basit <albasitdev@gmail.com>
2024-02-10 11:58:03 +0500

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,36 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const server = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const cookieParser = require('cookie-parser');
-const { createProduct } = require('./controller/Product');
-const productsRouter = require('./routes/Products');
-const categoriesRouter = require('./routes/Categories');
-const priceRangesRouter = require('./routes/PriceRanges');
-const ratingRangesRouter = require('./routes/RatingRanges');
-const brandsRouter = require('./routes/Brands');
-const usersRouter = require('./routes/Users');
-const authRouter = require('./routes/Auth');
-const cartRouter = require('./routes/Cart');
-const favoriteRouter = require('./routes/Favorite');
-const ordersRouter = require('./routes/Order');
-const { User } = require('./model/User');
-const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
-const path = require('path');
-
-
-
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const cookieParser = require("cookie-parser");
+const { createProduct } = require("./controller/Product");
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Categories");
+const priceRangesRouter = require("./routes/PriceRanges");
+const ratingRangesRouter = require("./routes/RatingRanges");
+const brandsRouter = require("./routes/Brands");
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const favoriteRouter = require("./routes/Favorite");
+const ordersRouter = require("./routes/Order");
+const { User } = require("./model/User");
+const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
+const path = require("path");
 
 const opts = {};
 opts.jwtFromRequest = cookieExtractor;
-opts.secretOrKey = process.env.JWT_SECRET_KEY; 
+opts.secretOrKey = process.env.JWT_SECRET_KEY;
 
-server.use(express.static(path.resolve(__dirname, 'dist')));
+server.use(express.static(path.resolve(__dirname, "build")));
 server.use(cookieParser());
 server.use(
   session({
@@ -42,47 +39,52 @@ server.use(
     saveUninitialized: false, // don't create session until something stored
   })
 );
-server.use(passport.authenticate('session'));
+server.use(passport.authenticate("session"));
 server.use(
   cors({
-    exposedHeaders: ['X-Total-Count'],
+    exposedHeaders: ["X-Total-Count"],
   })
 );
 server.use(express.json()); // to parse req.body
-server.use('/products', isAuth(), productsRouter.router);
-server.use('/categories', isAuth(), categoriesRouter.router);
-server.use('/priceRanges', isAuth(), priceRangesRouter.router);
-server.use('/ratingRanges', isAuth(), ratingRangesRouter.router);
-server.use('/brands', isAuth(), brandsRouter.router);
-server.use('/users', isAuth(), usersRouter.router);
-server.use('/auth', authRouter.router);
-server.use('/cart', isAuth(), cartRouter.router);
-server.use('/favorite', isAuth(), favoriteRouter.router);
-server.use('/orders', isAuth(), ordersRouter.router);
+server.use("/products", isAuth(), productsRouter.router);
+server.use("/categories", isAuth(), categoriesRouter.router);
+server.use("/priceRanges", isAuth(), priceRangesRouter.router);
+server.use("/ratingRanges", isAuth(), ratingRangesRouter.router);
+server.use("/brands", isAuth(), brandsRouter.router);
+server.use("/users", isAuth(), usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/favorite", isAuth(), favoriteRouter.router);
+server.use("/orders", isAuth(), ordersRouter.router);
 
 // Passport Strategies
 passport.use(
-  'local',
-  new LocalStrategy(
-    {usernameField:'email'},
-    async function (email, password, done) {
+  "local",
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    done
+  ) {
     try {
       const user = await User.findOne({ email: email });
       if (!user) {
-        return done(null, false, { message: 'invalid credentials' });
+        return done(null, false, { message: "invalid credentials" });
       }
       crypto.pbkdf2(
         password,
         user.salt,
         310000,
         32,
-        'sha256',
+        "sha256",
         async function (err, hashedPassword) {
           if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
-            return done(null, false, { message: 'invalid credentials' });
+            return done(null, false, { message: "invalid credentials" });
           }
-          const token = jwt.sign(sanitizeUser(user), process.env.JWT_SECRET_KEY);
-          done(null, {id:user.id, role:user.role, token }); 
+          const token = jwt.sign(
+            sanitizeUser(user),
+            process.env.JWT_SECRET_KEY
+          );
+          done(null, { id: user.id, role: user.role, token });
         }
       );
     } catch (err) {
@@ -92,7 +94,7 @@ passport.use(
 );
 
 passport.use(
-  'jwt',
+  "jwt",
   new JwtStrategy(opts, async function (jwt_payload, done) {
     try {
       const user = await User.findById(jwt_payload.id);
@@ -107,13 +109,11 @@ passport.use(
   })
 );
 
-
 passport.serializeUser(function (user, cb) {
   process.nextTick(function () {
     return cb(null, { id: user.id, role: user.role });
   });
 });
-
 
 passport.deserializeUser(function (user, cb) {
   process.nextTick(function () {
@@ -125,13 +125,13 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.MONGODB_URL);
-  console.log('database connected');
+  console.log("database connected");
 }
 
-server.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+server.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 server.listen(process.env.PORT, () => {
-  console.log('server started');
+  console.log("server started");
 });
